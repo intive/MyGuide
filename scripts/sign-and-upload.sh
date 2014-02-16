b1e471875c56c1504ecea36b4b01c4a09a8089a6
rkz <rafal.korzynski@blstream.com>
2014-02-16 03:07:39 +0100

    travis CI build configuration - delivering to TestFlightApp

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -12,4 +12,28 @@ fi
 PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
 OUTPUTDIR="$PWD/build/Release-iphoneos"
 
+
+echo "***************************"
+echo "*        Signing          *"
+echo "***************************"
+
 xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+
+zip -r -9 "$OUTPUTDIR/$APP_NAME.app.dSYM.zip" "$OUTPUTDIR/$APP_NAME.app.dSYM"
+
+RELEASE_DATE=`date '+%d-%m-%Y %H:%M:%S'`
+RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
+
+if [ ! -z "$TESTFLIGHT_TEAM_TOKEN" ] && [ ! -z "$TESTFLIGHT_API_TOKEN" ]; then
+  echo ""
+  echo "***************************"
+  echo "* Uploading to Testflight *"
+  echo "***************************"
+  curl http://testflightapp.com/api/builds.json \
+    -F file="@$OUTPUTDIR/$APP_NAME.ipa" \
+    -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
+    -F api_token="$TESTFLIGHT_API_TOKEN" \
+    -F team_token="$TESTFLIGHT_TEAM_TOKEN" \
+    -F distribution_lists='Internal' \
+    -F notes="$RELEASE_NOTES"
+fi
