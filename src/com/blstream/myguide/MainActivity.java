d4f32443bd88de341d23e871111498d1602b7c65
robin92pl <robin92pl@gmail.com>
2014-02-24 12:54:10 +0100

    Dropped appcombat and gridlayout libraries from repository and projects, properly merged with upstream (theme).

--- src/com/blstream/myguide/MainActivity.java
+++ src/com/blstream/myguide/MainActivity.java
@@ -3,9 +3,7 @@ package com.blstream.myguide;
 import android.app.Activity;
 import android.os.Bundle;
 
-import android.support.v7.app.ActionBarActivity;
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     /** Called when the activity is first created. */
     @Override
