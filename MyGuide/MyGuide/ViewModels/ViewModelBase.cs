d24c0777ad40537791df7bc1032ac37471630339
bukal260 <bukalski.piotr@gmail.com>
2014-02-24 23:30:01 +0100

    Added View and ViewModel base classes

---
+++ MyGuide/MyGuide/ViewModels/ViewModelBase.cs
@@ -0,0 +1,28 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+
+namespace MyGuide.ViewModels
+{
+    public class ViewModelBase : PropertyChangedBase
+    {
+        private INavigationService _navigation;
+
+        public ViewModelBase(INavigationService navigationService)
+        {
+            _navigation = navigationService;
+        }
+
+        public virtual void OnNavigatedFrom(NavigationMode navigationMode)
+        {
+        }
+
+        public virtual void OnNavigatedTo(NavigationMode navigationMode, bool isNewPageInstance)
+        {
+        }
+    }
+}
---
+++ MyGuide/MyGuide/Views/PageViewBase.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Phone.Controls;
+using MyGuide.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGuide.Views
+{
+    public class PageViewBase : PhoneApplicationPage
+    {
+        private bool _isNewPageInstance = false;
+
+        public PageViewBase()
+        {
+            _isNewPageInstance = true;
+        }
+
+        protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            ViewModelBase viewModel = _getViewModel();
+            viewModel.OnNavigatedFrom(e.NavigationMode);
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            ViewModelBase viewModel = _getViewModel();
+            viewModel.OnNavigatedTo(e.NavigationMode, _isNewPageInstance);
+            _isNewPageInstance = false;
+        }
+
+        private ViewModelBase _getViewModel()
+        {
+            var viewModel = this.DataContext as ViewModelBase;
+            if (viewModel != null)
+            {
+                return viewModel;
+            }
+            else
+            {
+                // Log that something went wrong
+                throw new InvalidOperationException("Page " + this.GetType() + " does not contain proper ViewModel in DataContext");
+            }
+        }
+    }
+}
