5341ad9a60c46a01b41661360b262858a54ecff1
adamwalocha <adam.walocha@blstream.com>
2014-02-24 15:25:01 +0100

    MYGUIDE-54 As a product owned i want to be able to download latest build from CI (WP) MYGUIDE-67 Add project to CI also added gitignore

---
+++ build_vs2013_WP8.cmd
@@ -0,0 +1,32 @@
+@echo off
+echo ---
+echo Configure VS2013
+echo ---
+call "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat" x86 || goto error
+
+set CURRENT_DIR=%CD%
+
+
+echo ---
+echo Building
+echo ---
+cd %CURRENT_DIR%\MyGuide
+msbuild MyGuide.sln /t:rebuild /p:Configuration="Release" || goto error
+msbuild MyGuide.sln /t:rebuild /p:Configuration="Debug" || goto error
+
+goto end
+
+:error
+echo ---
+echo --- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ---
+echo ---
+echo ---
+echo ------------- FAILED TO BUILD: %MODULE_NAME%
+echo ---
+echo ---
+echo --- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ---
+echo ---
+cd %CURRENT_DIR%
+exit /b 1
+
+:end
