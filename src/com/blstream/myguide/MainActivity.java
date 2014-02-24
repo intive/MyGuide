f37a2705401d3383c76a041ccfe5266caf7ad111
ampatalas <a.m.patalas@gmail.com>
2014-02-24 19:05:05 +0100

    add onClick to buttons in activity_main.xml edit AndroidManifest.xml add res/menu/menu_main.xml and res/layout/activity_how_to_get.xml add AboutActivity.java, HowToGetActivity.java, OptionsActivity.java, SightseeingActivity.java

--- src/com/blstream/myguide/MainActivity.java
+++ src/com/blstream/myguide/MainActivity.java
@@ -1,7 +1,12 @@
 package com.blstream.myguide;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity
 {
@@ -11,5 +16,49 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    
+    /** Sets options menu. Called when options button is first clicked.*/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    
+    /** Called when one of items in options menu is clicked.*/
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.options_menu:
+                goOptions();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    
+    /** Called when btnGoTickets ("Tickets and times") is clicked. Opens activity with information about the Zoo. */
+    public void goAbout(View view){
+    	Intent intent = new Intent(this, AboutActivity.class);
+    	startActivity(intent);
+    }
+    
+    /** Called when btnSightseeing ("Sightseeing") is clicked. Opens activity with sightseeing chooser screen. */
+    public void goSightseeing(View view){
+    	Intent intent = new Intent(this, SightseeingActivity.class);
+    	startActivity(intent);
+    }
+    
+    
+    /** Called when btnHowToGet ("How to get") is clicked. Opens activity that shows how to get to the Zoo. */
+    public void goHowToGet(View view){
+    	Intent intent = new Intent(this, HowToGetActivity.class);
+    	startActivity(intent);
+    }
+    
+    /** Opens activity with options screen when "Options" is clicked in options menu. */
+    private void goOptions(){
+    	Intent intent = new Intent(this, OptionsActivity.class);
+    	startActivity(intent);
     }
 }
