65566af1d4a0530e0ae63156f9d3f7af1216d6ae
bukal260 <bukalski.piotr@gmail.com>
2014-02-25 23:04:56 +0100

    Added DesignData namespace Added LoremImpusGenerator

---
+++ MyGuide/MyGuide/DesignData/LoremImpusGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGuide.DesignData
+{
+    public static class LoremImpusGenerator
+    {
+        public static string Generate(int lines)
+        {
+            #region LoremIpsum
+            const string LoremIpsum = @"Lorem ipsum dolor sit amet,
+consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            #endregion LoremIpsum
+
+            return String.Join(
+                Environment.NewLine,
+                Enumerable.Repeat(LoremIpsum, lines));
+        }
+    }
+}
