a57f36a39a5785af2d1cc9c4996f6a53c397922d
Kamil Lelonek <squixy.sln@gmail.com>
2014-03-02 20:08:38 +0100

    Showing zoo, animals and current position on map

---
+++ MyGuide/MapViewController.h
@@ -0,0 +1,19 @@
+//
+//  MapViewController.h
+//  MyGuide
+//
+//  Created by Kamil Lelonek on 3/2/14.
+//  Copyright (c) 2014 BLStream - Rafał Korżyński. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import "Settings.h"
+#import "AFParsedData.h"
+#import "AFAnimal.h"
+#import "MKAnnotationAnimal.h"
+
+@interface MapViewController : UIViewController
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+- (IBAction) centerOnCurrentLocation: (id) sender;
+@end
---
+++ MyGuide/MapViewController.m
@@ -0,0 +1,49 @@
+//
+//  MapViewController.m
+//  MyGuide
+//
+//  Created by Kamil Lelonek on 3/2/14.
+//  Copyright (c) 2014 BLStream - Rafał Korżyński. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@implementation MapViewController {
+    Settings     *_settings;
+    AFParsedData *_data;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    _settings = [Settings sharedSettingsData];
+    _data     = [AFParsedData sharedParsedData];
+    
+    [self showUserPosition];
+    [self showAnimals];
+    [self centerMap];
+}
+
+- (IBAction) centerOnCurrentLocation: (id) sender {
+    CLLocation *userLocation = self.mapView.userLocation.location;
+    if(userLocation) {
+        [self.mapView setCenterCoordinate: userLocation.coordinate];
+    }
+}
+
+- (void) showUserPosition {
+    if(_settings.showUserPosition) {
+        [self.mapView setShowsUserLocation: YES];
+    }
+}
+
+- (void) showAnimals {
+    NSArray *animals = [MKAnnotationAnimal buildAnimalMKAnnotations: _data.animalsArray];
+    [self.mapView addAnnotations: animals];
+}
+
+- (void) centerMap {
+    [self.mapView setRegion: _settings.mapBounds animated:YES];
+}
+
+@end
