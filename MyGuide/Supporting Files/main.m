fb1954ae265cb7f2c5496361552c55ca48019b27
rkz <rafal.korzynski@blstream.com>
2014-03-06 16:50:05 +0100

    [CI update] Solving problems with MSLog Level & Provisioning profile

--- MyGuide/Supporting Files/main.m
+++ MyGuide/Supporting Files/main.m
@@ -13,6 +13,10 @@
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
+    
+#warning @"Remove this in AppStore build - Is used only for Testing"
+        asl_set_filter(NULL, ASL_FILTER_MASK_UPTO(ASL_LEVEL_DEBUG+1));
+        
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
