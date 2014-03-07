fb058c8bcb9d0f14952d16ada3c1ad0757a4e792
bukal260 <bukalski.piotr@gmail.com>
2014-03-07 20:59:48 +0100

    Added MessageDialogService class

---
+++ MyGuide/MyGuide/Services/MessageDialogService.cs
@@ -0,0 +1,22 @@
+﻿using MyGuide.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGuide.Services
+{
+    public class MessageDialogService : IMessageDialogService
+    {
+        public Task<bool> ShowDialog(string title, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ShowDialog(string title, string message, string leftButton, string rightButton)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
