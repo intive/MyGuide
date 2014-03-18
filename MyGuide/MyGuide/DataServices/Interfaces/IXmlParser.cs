a653805c2d404486023db99e54d3b5f4dafda761
bukal260 <bukalski.piotr@gmail.com>
2014-03-18 17:30:25 +0100

    Moved interfaces files to separate folder

---
+++ MyGuide/MyGuide/DataServices/Interfaces/IXmlParser.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace MyGuide.DataServices.Interfaces
+{
+    public interface IXmlParser<T>
+    {
+        Task<T> DeserializeXml(string dataPath);
+
+        string SerializeXml(T objectToSerialize);
+    }
+}
