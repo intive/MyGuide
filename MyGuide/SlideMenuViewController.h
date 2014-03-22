d11fb1a7643db903b043e8f45471af65f81ade55
Kamil Lelonek <squixy.sln@gmail.com>
2014-03-22 21:57:53 +0100

    Created SlideMenu skeleton

---
+++ MyGuide/SlideMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  SlideMenuViewController.h
+//  MyGuide
+//
+//  Created by Kamil Lelonek on 3/22/14.
+//  Copyright (c) 2014 - Open Source (Apache 2.0 license). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SlideMenuViewController : UIViewController
+
+@end
