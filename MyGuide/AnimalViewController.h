67a4a4f9cabc07194eb0091c9734c1661ae5afbb
Kamil Lelonek <squixy.sln@gmail.com>
2014-03-23 15:18:34 +0100

    Animals list implemented

---
+++ MyGuide/AnimalViewController.h
@@ -0,0 +1,19 @@
+//
+//  AnimalViewController.h
+//  MyGuide
+//
+//  Created by Kamil Lelonek on 3/23/14.
+//  Copyright (c) 2014 - Open Source (Apache 2.0 license). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SWRevealViewController.h"
+#import "AFParsedData.h"
+#import "AFAnimal.h"
+
+@interface AnimalViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
