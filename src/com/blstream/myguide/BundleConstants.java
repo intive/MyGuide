06161caaaff5e3352ac57a2141aceec55cb24392
Piotrek <p.swiatowski@gmail.com>
2014-03-27 10:37:40 +0100

    Prepare animal description activity

---
+++ src/com/blstream/myguide/BundleConstants.java
@@ -0,0 +1,11 @@
+
+package com.blstream.myguide;
+
+/**
+ * Created by Piotrek on 2014-03-27.
+ */
+public class BundleConstants {
+
+	// TAGS
+	public static final String SELECTED_ANIMAL = "selected_animal";
+}
