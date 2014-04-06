0cb9a3c76b048e5c3b7110fe19db5b0af3222301
Kamil Lelonek <squixy.sln@gmail.com>
2014-04-06 14:42:01 +0200

    Map for GastronomyDetails implemented

--- MyGuide/GastronomyDetailsViewController.m
+++ MyGuide/GastronomyDetailsViewController.m
@@ -10,6 +10,8 @@
 
 @interface GastronomyDetailsViewController ()
 
+@property (strong, nonatomic) UIViewController *detailsMapController;
+
 @end
 
 @implementation GastronomyDetailsViewController
@@ -23,12 +25,32 @@
 {
     [super viewDidLoad];
     [self setTitle: [NSString stringWithFormat: NSLocalizedString(@"cellLabelRestaurant", nil), _restaurantID]];
+    [_segmentedControl addTarget:self action:@selector(alternateBetweenContent) forControlEvents:UIControlEventValueChanged];
+    [self prepareMapController];
 }
 
 
 - (void) viewWillAppear: (BOOL)animated
 {
     [_segmentedControl setSelectedSegmentIndex:0];
+}
+
+- (void) prepareMapController
+{
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]];
+    _detailsMapController = [storyboard instantiateViewControllerWithIdentifier:@"detailsMap"];
+}
+
+- (void)alternateBetweenContent
+{
+    NSInteger selectedIndex = [_segmentedControl selectedSegmentIndex];
+    if(selectedIndex == 0) {
+    }
+    else if(selectedIndex == 1) {
+    }
+    else {
+        [self.navigationController pushViewController:_detailsMapController animated:YES];
+    }
 }
 
 @end
