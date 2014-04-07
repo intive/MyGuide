12151bb2b8adfc8d63a5cc9ef62ba2ebd9907e99
Kamil Lelonek <squixy.sln@gmail.com>
2014-04-07 13:56:36 +0200

    Gastronomy DishCell created

---
+++ MyGuide/Gastronomy/DishTableViewCell.m
@@ -0,0 +1,22 @@
+//
+//  DishTableViewCell.m
+//  MyGuide
+//
+//  Created by Kamil Lelonek on 4/7/14.
+//  Copyright (c) 2014 - Open Source (Apache 2.0 license). All rights reserved.
+//
+
+#import "DishTableViewCell.h"
+
+@implementation DishTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    return [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+}
+
+- (void) awakeFromNib
+{
+}
+
+@end
