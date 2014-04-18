3bb5206ccb2e44728577f617fd90a18c5b0a51df
MDikkii <mdemkow@gmail.com>
2014-04-18 17:59:44 +0200

    First try of user position on map

--- MyGuide/MyGuide/ViewModels/SightseeingPageViewModel.cs
+++ MyGuide/MyGuide/ViewModels/SightseeingPageViewModel.cs
@@ -1,30 +1,45 @@
 ﻿using Caliburn.Micro;
+using Microsoft.Phone.Controls.Maps.Platform;
 using MyGuide.DataServices.Interfaces;
 using MyGuide.Models;
 using MyGuide.Services.Interfaces;
 using System;
+using System.Device.Location;
+using System.Windows.Media;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace MyGuide.ViewModels
 {
     public class SightseeingPageViewModel : ViewModelBase
     {
+        private GeoCoordinate _userPositionLocation;
+
         public SightseeingPageViewModel(INavigationService navigationService,
             IMessageDialogService messageDialogService, IDataService dataService, IOptionsService optionService)
             : base(navigationService, messageDialogService, dataService, optionService)
         {
         }
 
+        public GeoCoordinate UserPositionLocation
+        {
+            get { return _userPositionLocation; }
+            set { _userPositionLocation = value; NotifyOfPropertyChange(() => UserPositionLocation); }
+        }
+
         #region Commands
         //Uncomment all method and set some instructions when we'll implement appbar clickable
         //Remeber to add icons for appbar buttons!
 
-        //public void ShowMap()
-        //{
-        //}
+        public void ShowAnimals()
+        {
+            UserPositionLocation = new GeoCoordinate(51.104942, 17.073820);
+        }
 
-        //public void ShowAnimals()
-        //{
-        //}
+        public void ShowMap()
+        {
+            UserPositionLocation = new GeoCoordinate(51.104942, 17.073520);
+        }
 
         //public void ShowMap()
         //{
@@ -47,5 +62,10 @@ namespace MyGuide.ViewModels
         //}
 
         #endregion Commands
+
+        public override void OnNavigatedTo(NavigationMode navigationMode, bool isNewPageInstance)
+        {
+            UserPositionLocation = new GeoCoordinate(51.104642, 17.073520);
+        }
     }
 }
