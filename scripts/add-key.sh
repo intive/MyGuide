d35719edb1bd1bf2982800cf11e2ea1087d8bf03
rkz <rafal.korzynski@blstream.com>
2014-04-29 15:01:32 +0200

    set default keychain after it has been created.

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -16,6 +16,8 @@ openssl aes-256-cbc -k "$BLS_ENCRYPTION_SECRET" -in scripts/certs/dist.cer.enc -
 openssl aes-256-cbc -k "$BLS_ENCRYPTION_SECRET" -in scripts/certs/dist.p12.enc -d -a -out ./$BLS_TMP/dist.p12
 
 security create-keychain -p travis ios-build.keychain
+security default-keychain -s ios-build.keychain
+security unlock-keychain -p travis ios-build.keychain
 security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./$BLS_TMP/dist.cer -k  ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./$BLS_TMP/dist.p12 -k  ~/Library/Keychains/ios-build.keychain -P $BLS_KEY_PASSWORD -T /usr/bin/codesign
