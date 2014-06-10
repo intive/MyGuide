0bf6b6519482f5164c7da38c6eccbef03699716d
Kamil Lelonek <squixy.sln@gmail.com>
2014-06-10 15:51:00 +0200

    Minor UX fix: colors and sliding menus

--- MyGuide/TrackSlideMenuViewController.m
+++ MyGuide/TrackSlideMenuViewController.m
@@ -28,10 +28,7 @@
     UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320.0f, 44.0f)];
     headerView.backgroundColor = [UIColor colorWithRed:1.0f green:0.584f blue:0.0f alpha:1.0f];
     self.tableView.tableHeaderView = headerView;
-    
-    UIView *footerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320.0f, 1.0f)];
-    footerView.backgroundColor = [UIColor whiteColor];
-    self.tableView.tableFooterView = footerView;
+    self.tableView.tableFooterView = [[UIView alloc] init];
     
     CALayer *bgLayer = [CALayer layer];
     bgLayer.backgroundColor = headerView.backgroundColor.CGColor;
